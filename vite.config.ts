e5f7e8ad945ea950b3d98d495f25d5c96cb9fd53
khanhdip <khanhdip@gmail.com>
2026-06-25 08:47:26 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/WEBSITE_DEMO1/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
